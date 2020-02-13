7ab6cf56a63975c61701a5bf9e4c296865909d6a
Nicholas Buzzerio <nick.buzzerio@gmail.com>
2020-02-12 23:56:12 -0500

    cleaned up front-end styling, images, names, and adoption element are correctly spaced and centered

--- client/src/components/dogs.js
+++ client/src/components/dogs.js
@@ -5,6 +5,7 @@ import style from 'styled-components'
 
 const StyledDiv = style.div`
   width: 80%;
+  height: 300px;
   margin: auto;
 `;
 
--- client/src/components/meetThem.js
+++ client/src/components/meetThem.js
@@ -4,41 +4,70 @@ import style from 'styled-components';
 var StyledDiv = style.div`
   background-color: #6504b5;
   text-align: center;
+  margin: .5em;
   color: white;
   box-sizing: border-box;
   border: 1px outset black;
-  width: 290;
-  height: 360;
+  border-radius:10px;
+  width: 228px;
+  height: 300px;
+  overflow: auto;
   float: left;
-  boxSizing: border-box;
+  line-height: 125%;
+  position: relative;
+`;
+
+var StyledInfo = style.div`
+  background-color: #6504b5;
+  text-align: center;
+  color: white;
+  box-sizing: border-box;
+  border: 1px outset black;
+  width: 100%;
+  height: 80%;
+  overflow: auto;
+  float: left;
+  line-height: 150%;
   position: relative;
 `;
 
 var StyledSubDiv = style.div`
   background-color: #6504b5;
   text-align: center;
+  border-radius:0px 0px 10px 10px;
   color: white;
-  width: 290;
+  width: 100%;
+  height: 20%;
+  box-sizing: border-box;
+  position: relative;
+  border-top: 1px solid black;
+  overflow: hidden;
+  &:hover {
+    background-color: #330066
+  }
 `;
 
 function MeetThem(props) {
 
   return(
     <StyledDiv>
-      <br></br>
-      <br></br>
-      <img src='https://availabledogphotos.s3.amazonaws.com/paw.png'></img>
-      <br></br>
-      <br></br>
-      <br></br>
-      {Math.max(props.dogs.length - 4, 0)} more {props.breed}s <br></br>
-      available for adoption
-      <hr></hr>
-      <br></br>
+      <StyledInfo>
+        <br></br>
+        <br></br>
+        <img src='https://availabledogphotos.s3.amazonaws.com/paw.png'></img>
+        <br></br>
+        <br></br>
+        {Math.max(props.dogs.length - 4, 0)} more {props.breed}s
+        available for adoption
+        <br></br>     
+      </StyledInfo>
       <StyledSubDiv>
+        <br></br>
         Meet Them
+        <br></br>
+        <br></br>
       </StyledSubDiv>
-      <br></br>
+      
     </StyledDiv>
   )
 }
