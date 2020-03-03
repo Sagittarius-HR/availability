cad74f8baea2d260f52319c8c6da04b0555db5e0
Nicholas Buzzerio <nick.buzzerio@gmail.com>
2020-03-03 00:03:00 -0500

    migrated local database to mongoAtlas cloud database

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost/breedAvailability', { useNewUrlParser: true }, (err) => {
+const db = mongoose.connect('mongodb+srv://nbuzzer:77mongoDB77@fec-availability-mdczx.mongodb.net/FEC', { useNewUrlParser: true }, (err) => {
   if (err) { throw err; };
   console.log('Availability Database connection was successful.')
 });
